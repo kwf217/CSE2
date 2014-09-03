10e02e1506531edfdca0a49bede130a12b251fee
kwf217 <kwf217@lehigh.edu>
2014-09-03 01:38:49 +0000

    updated hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////
+//Kevin Fogarty
+//kwf217
+//CSE2
+//Hello World Java Program
+//
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+    public static void main(String[] args) {
+    
+//  print the statement with a box around it
+
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-K--W--F--2--1--7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("The past couple summers");
+    System.out.println("my mom sent me to foreign");
+    System.out.println("countries and I had to");
+    System.out.println("figure out how to live");
+    System.out.println("on my own. I love going");
+    System.out.println("on adventures.");
+
+//print brief bio
+
+    }
+    
+}
+  
+
+
+ 
+ 
+ 
+  
+  
